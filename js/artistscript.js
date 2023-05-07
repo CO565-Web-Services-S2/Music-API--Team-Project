e4a886d8ff78550563577e04bdaacc8382819c5b
SudathNawagamuwage21 <91678915+SudathNawagamuwage21@users.noreply.github.com>
2023-05-07 10:28:48 +0100

    updated

--- js/artistscript.js
+++ js/artistscript.js
@@ -1,16 +1,20 @@
 
+// get references to the search button, search input, and results container
 const searchButton = document.getElementById("search-button");
 const searchInput = document.getElementById("search-input");
 const resultsContainer = document.getElementById("results-container");
 
+// add an event listener to the search button that executes when it's clicked
 searchButton.addEventListener("click", () => {
     const artistId = searchInput.value.trim();
     if (artistId !== "") {
+      // fetch data from the AudioDB API using the artist ID
       fetch(`https://theaudiodb.com/api/v1/json/2/artist.php?i=${artistId}`)
         .then((response) => {
           if (!response.ok) {
             throw new Error("Network response was not ok");
           }
+          // convert the response to JSON format
           return response.json();
         })
         .then((data) => {
