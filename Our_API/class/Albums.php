00cc9ef54fc9cfa316b056d97f406a0332eb67df
kwhynn1 <91710013+kwhynn1@users.noreply.github.com>
2023-05-11 18:29:17 +0100

    Update Albums.php

--- Our_API/class/Albums.php
+++ Our_API/class/Albums.php
@@ -19,22 +19,4 @@ class album
         return $result;	
     }
 
-    function create() 
-    {
-        $stmt = $this->conn->prepare("INSERT INTO album (`ArtistID`, `Album`, `Released`, `Sales`, ArtistName) VALUES (?,?,?,?,?)");
-        
-        $this->ArtistID = htmlspecialchars(strip_tags($this->ArtistID));
-        $this->Album = htmlspecialchars(strip_tags($this->Album));
-        $this->Released = htmlspecialchars(strip_tags($this->Released));
-        $this->Sales = htmlspecialchars(strip_tags($this->Sales));
-        $this->ArtistName = htmlspecialchars(strip_tags($this->ArtistName));
-        
-        $stmt->bind_param( $this->ArtistID, $this->Album, $this->Released, $this->Sales,$this->ArtistName);
-        
-        if($stmt->execute()) {
-            return true;
-        }
-        return false;		 
-    }
-    
 }
